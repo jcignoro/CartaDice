efb24a05082e54fc3bbc6daab271e35ee5a4f869
NITRO <devart@Devart>
2023-10-18 18:44:16 +0800

    first commit

---
+++ src/pages/ComingSoonLP.js
@@ -0,0 +1,74 @@
+import "./ComingSoonLP.css";
+
+const ComingSoonLP = () => {
+  return (
+    <div className="desktop-1">
+      <div className="under-construction">
+        <img className="artboard-1-1" alt="" src="/artboard-1-1@2x.png" />
+        <div className="carta-dice">© 2023 Carta Dice | Cartadice.com</div>
+        <div className="frame-parent">
+          <div className="group-wrapper">
+            <div className="rectangle-parent">
+              <div className="rectangle-parent" />
+              <img className="group-icon" alt="" src="/group.svg" />
+            </div>
+          </div>
+          <img className="discord-1-icon" alt="" src="/discord-1.svg" />
+          <div className="group-wrapper">
+            <div className="rectangle-parent" />
+            <img className="group-icon1" alt="" src="/group1.svg" />
+          </div>
+        </div>
+        <img className="group-icon2" alt="" src="/group2.svg" />
+        <img className="icon" alt="" src="/.svg" />
+        <div className="group-parent">
+          <div className="rectangle-container">
+            <div className="group-item" />
+            <div className="group-container">
+              <div className="join-our-discord-wrapper">
+                <div className="join-our-discord">Join our Discord</div>
+              </div>
+            </div>
+          </div>
+          <img className="group-inner" alt="" src="/group-366.svg" />
+        </div>
+        <img className="layer-1-icon" alt="" src="/layer-1.svg" />
+        <img className="under-construction-child" alt="" src="/group-382.svg" />
+        <div className="masters-of-horror">{`masters OF HORROR AND DARK FANTASY `}</div>
+        <img className="under-construction-item" alt="" src="/group-381.svg" />
+        <img className="group-icon3" alt="" src="/group3.svg" />
+        <img className="group-icon4" alt="" src="/group4.svg" />
+        <img className="group-icon5" alt="" src="/group5.svg" />
+        <img className="group-icon6" alt="" src="/group6.svg" />
+        <img className="group-icon7" alt="" src="/group7.svg" />
+        <img className="group-icon8" alt="" src="/group8.svg" />
+        <img className="group-icon9" alt="" src="/group9.svg" />
+        <div className="in-the-shadowed">
+          In the shadowed depths of the creative abyss, where terror intertwines
+          with imagination, and darkness births unholy wonders.
+        </div>
+        <div className="coming-soon">COMING soon...</div>
+      </div>
+      <div className="group-div">
+        <div className="rectangle-parent1">
+          <div className="rectangle-div" />
+          <div className="group-frame">
+            <div className="twitter-wrapper">
+              <div className="join-our-discord">Twitter</div>
+            </div>
+          </div>
+        </div>
+        <img className="group-child1" alt="" src="/group-3661.svg" />
+      </div>
+      <img className="group-icon10" alt="" src="/group10.svg" />
+      <img
+        className="screen-shot-2023-10-16-at-151"
+        alt=""
+        src="/screen-shot-20231016-at-151-1@2x.png"
+      />
+      <img className="desktop-1-child" alt="" src="/vector-1.svg" />
+    </div>
+  );
+};
+
+export default ComingSoonLP;
